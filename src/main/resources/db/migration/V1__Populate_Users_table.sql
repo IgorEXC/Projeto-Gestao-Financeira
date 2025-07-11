49e06fd26742ea6b7355775c86b06f62ec40d8b7
Igor Vinnícius <igorquintao22@gmail.com>
2025-07-10 21:34:00 -0300

    Primeira migration e configuração no application.yml

---
+++ src/main/resources/db/migration/V1__Populate_Users_table.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_user (monthly_income, cpf, name, username, email) VALUES
+(6000.14, '12340987112', 'Igor Vinnicius', 'Galodoidorep', 'igor@gmail.com'),
+(8900.63, '12345325312', 'Maria Brenda', 'FoxDarkness', 'fox@gmail.com'),
+(1000.45, '29843736289', 'Nicolau Veloso', 'JuliusPedriguin', 'maquiavel@gmail.com'),
+(5000.98, '12345678909', 'Marcus Antunes', 'aniquilador4691', 'marcus@outlook.com'),
+(2000.12, '09876543211', 'Guilherme Guimarães', 'galego', 'galego@firma.com.br'),
+(1560.56, '11223344556', 'Alice Barbado', 'Parrudo56', 'barbie12@gmail.com'),
+(14000.00, '00998877665', 'Pedro Ivo', 'jotaJOTINHA', 'jjjj_@yahoo.com'),
+(400095.34, '33221155667', 'Deryck Henrique', 'anderlaine', 'henri.der@gov.br'),
+(13212.99, '33999661904', 'Lucas Montano', 'Ivobespe', 'montanhaAbdominal@gmail.com.br'),
+(3500.10, '28730497185', 'Jefferson Rodrigues', 'Macarronese', 'aleatorio99@zoho.com');
