1749d1195f7e06576c4ef57f3f07ab21b86d7800
Igor Vinnícius <igorquintao22@gmail.com>
2025-07-12 12:07:45 -0300

    Correções migration e exceções

--- src/main/java/com/financas/gestaofinanceira/GestaoFinanceiraApplication.java
+++ src/main/java/com/financas/gestaofinanceira/GestaoFinanceiraApplication.java
@@ -16,4 +16,6 @@ public class GestaoFinanceiraApplication {
 
 /*Implementar despesas atrasadas, despesas pagas, despesas futuras. Planos e projetos
 * ordem de compras pagas ou pendentes, investimentos, gastos essencias, gastos futeis
-* fazer excel e relatorio jasper*/
+* fazer excel e relatorio jasper
+* tratar melhor as excecoes, qnd acontece uma excecao por causa do cpf ele nao avisa
+* ver por que data_atualizacao da auditoria nao esta funcionando*/
