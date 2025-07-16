57018a4d1d89f39199afada5f8c997d582b78e2d
Igor Vinnícius <igorquintao22@gmail.com>
2025-07-16 15:51:00 -0300

    Criando anotações para encapsular os Content-Type

--- src/main/java/com/financas/gestaofinanceira/resources/UserResource.java
+++ src/main/java/com/financas/gestaofinanceira/resources/UserResource.java
@@ -1,19 +1,16 @@
 package com.financas.gestaofinanceira.resources;
 
-import com.financas.gestaofinanceira.services.UserService;
-import com.financas.gestaofinanceira.domain.User;
+import com.financas.gestaofinanceira.annotations.GETMultiFormat;
+import com.financas.gestaofinanceira.annotations.POSTMultiFormat;
+import com.financas.gestaofinanceira.annotations.PUTMultiFormat;
 import com.financas.gestaofinanceira.domain.dto.UserRequestDTO;
 import com.financas.gestaofinanceira.domain.dto.UserResponseDTO;
-import com.financas.gestaofinanceira.domain.mapper.UserMapper;
+import com.financas.gestaofinanceira.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -30,34 +26,18 @@ public class UserResource {
 
 	private final UserService service;
 
-	@GetMapping(value = "/all",
-			produces = {
-					MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_YAML_VALUE})
+	@GETMultiFormat(value = "/all")
 	public ResponseEntity<CollectionModel<UserResponseDTO>> findAllPerPage(@RequestParam int page, @RequestParam int itensPerPage){
 		return ResponseEntity.ok().body(service.findAllPerPage(page, itensPerPage));
 	}
 
-	@GetMapping(value = "/{id}",
-			produces = {
-					MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_YAML_VALUE})
+	@GETMultiFormat(value = "/{id}")
 	public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id){
 		UserResponseDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 
-	@PostMapping(value = "/create",
-			produces = {
-					MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_YAML_VALUE},
-			consumes = {
-					MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_YAML_VALUE})
+	@POSTMultiFormat(value = "/create")
 	public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO dto) {
 		UserResponseDTO responseDto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -65,15 +45,7 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(responseDto);
 	}
 
-	@PutMapping(value = "/update/{id}",
-			produces = {
-					MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_YAML_VALUE},
-			consumes = {
-					MediaType.APPLICATION_JSON_VALUE,
-					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_YAML_VALUE})
+	@PUTMultiFormat(value = "/update/{id}")
 	public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserRequestDTO dto){
 		return ResponseEntity.ok().body(service.update(id, dto));
 	}
