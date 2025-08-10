b135994917f27cc0ec1346fc32207e136acc5c70
Igor Vinnícius <igorquintao22@gmail.com>
2025-08-09 22:33:41 -0300

    Começando a ajustar Migrations

---
+++ src/main/resources/db/migration/V1__Create_User_table.sql
@@ -0,0 +1,16 @@
+create table tb_user
+(
+    user_id          bigserial
+        constraint tb_user_pk
+            primary key,
+    cpf              varchar(11) not null,
+    name             varchar(40),
+    username         varchar(40),
+    email            varchar(50) not null,
+    password         varchar(40),
+    birthdate        date,
+    monthly_income   decimal,
+    data_atualizacao timestamp,
+    data_inclusao    timestamp
+);
+
