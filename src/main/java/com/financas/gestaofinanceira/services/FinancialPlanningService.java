b386cc6337a5e3926ef59d168fa1371869590390
IgorEXC <igorvinnie15@gmail.com>
2025-10-15 21:31:32 -0300

    Começando a criar plano financeiro

--- src/main/java/com/financas/gestaofinanceira/services/FinancialPlanningService.java
+++ src/main/java/com/financas/gestaofinanceira/services/FinancialPlanningService.java
@@ -1,6 +1,7 @@
 package com.financas.gestaofinanceira.services;
 
 import com.financas.gestaofinanceira.domain.FinancialPlanning;
+import com.financas.gestaofinanceira.domain.dto.FinancialPlanningRequestDTO;
 import com.financas.gestaofinanceira.domain.dto.FinancialPlanningResponseDTO;
 import com.financas.gestaofinanceira.domain.mapper.FinancialPlanningMapper;
 import com.financas.gestaofinanceira.repositories.FinancialPlanningRepository;
@@ -17,6 +18,7 @@ public class FinancialPlanningService {
 
 	private final FinancialPlanningRepository repository;
 	private final FinancialPlanningMapper mapper;
+    private final UserService userService;
 
 	public List<FinancialPlanningResponseDTO> findAll(){
 		List<FinancialPlanning> list = repository.findAll();
@@ -27,4 +29,15 @@ public class FinancialPlanningService {
 		FinancialPlanning obj = repository.findById(id).orElseThrow();
 		return mapper.entityToResponse(obj);
 	}
+
+    public FinancialPlanningResponseDTO findById(Long userId, FinancialPlanningRequestDTO dto) {
+        Long user = userService.findById(userId).getId();
+        //cadastrar um plano financeiro para um usuario pelo id do usuario
+        return null;
+
+    }
+
+    private Double MonthlyEconomicsGoal(Long userId, FinancialPlanningResponseDTO dto) {
+        return 0.0;
+    }
 }
