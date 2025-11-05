367184c9d5d670e1f168b2a7fb6e0643395645e8
IgorEXC <igorvinnie15@gmail.com>
2025-11-04 22:38:51 -0300

    Implementando Paginação para findAll de Expenses

--- src/main/java/com/financas/gestaofinanceira/repositories/ExpenseRepository.java
+++ src/main/java/com/financas/gestaofinanceira/repositories/ExpenseRepository.java
@@ -1,13 +1,18 @@
 package com.financas.gestaofinanceira.repositories;
 
 import com.financas.gestaofinanceira.domain.dto.projections.ExpenseCategoryProjection;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.financas.gestaofinanceira.domain.Expense;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import org.springframework.data.domain.Pageable;
+
 public interface ExpenseRepository extends JpaRepository<Expense, Long>{
+
+    Page<Expense> findAll(Pageable pageable);
 
     @Query("""
     SELECT 
--- src/main/java/com/financas/gestaofinanceira/resources/ExpenseResource.java
+++ src/main/java/com/financas/gestaofinanceira/resources/ExpenseResource.java
@@ -8,14 +8,11 @@ import com.financas.gestaofinanceira.domain.dto.request.ExpenseRequestDTO;
 import com.financas.gestaofinanceira.domain.dto.response.ExpenseResponseDTO;
 import com.financas.gestaofinanceira.domain.mapper.ExpenseMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -26,8 +23,10 @@ public class ExpenseResource {
 	private final ExpenseMapper mapper;
 
 	@GETMultiFormat
-	public ResponseEntity<List<ExpenseResponseDTO>> findAll(){
-		return ResponseEntity.ok().body(service.findAll());
+	public ResponseEntity<Page<ExpenseResponseDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size){
+		return ResponseEntity.ok().body(service.findAll(page, size));
 	}
 
 	@GETMultiFormat(value = "/{id}")
